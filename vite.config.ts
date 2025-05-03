0379c1f9f93ec1f09526aa670b6b9297d921735a
thealienseb <sebastianramos5387@gmail.com>
2025-05-03 10:25:30 -0400

    Temp fix with vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,22 +4,13 @@ import react from "@vitejs/plugin-react-swc";
 import { crx } from "@crxjs/vite-plugin";
 import manifest from "./src/manifest";
 import path from "path";
-<<<<<<< HEAD
-// import { crx } from "@crxjs/vite-plugin";   // <- NEW
-import manifest from "./public/manifest.json";     // <- NEW
-=======
->>>>>>> c60fcd8c11dd508e77ea9f1e2df8438fb16e647c
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     mode === "development" && componentTagger(),
-<<<<<<< HEAD
-    // crx({ manifest })                         // <- NEW
-=======
     crx({ manifest })
->>>>>>> c60fcd8c11dd508e77ea9f1e2df8438fb16e647c
   ].filter(Boolean),
 
   resolve: {
